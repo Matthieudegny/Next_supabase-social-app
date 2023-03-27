d47585a0f259beb5ec0b7084f2fde09e5e0256a0
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-03-27 05:40:05 +0200

    fixe bugg release v2

--- pages/login.js
+++ pages/login.js
@@ -11,7 +11,7 @@ export default function LoginPage() {
         provider: "google",
       },
       {
-        redirectTo: window.location.origin,
+        redirectTo: "https://share-your-prompt.vercel.app/",
       }
     );
   }
